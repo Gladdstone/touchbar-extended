6dc09b1c23908b0ea8c96fce8f265fd2e0662e89
Joseph Farrell <josephfarrell@Josephs-MacBook-Pro-2.local>
2021-05-04 00:46:29 -0400

    initial display function generation

--- generate.py
+++ generate.py
@@ -16,11 +16,27 @@ with open('config.json') as config:
 tmpdir = tempfile.mkdtemp()
 plugin_file = open('tmp/touchbar_extended.plugin.zsh', 'w+')
 
-for key in json_config['default']:
-    key_value = key
-    key_text = json_config['default'][key]['text']
-    function_name = json_config['default'][key]['command']
-    build_key(file, key_value, key_text, function_name)
+def display_function(plugin_file):
+    definition = r'function display() {'
+    initial_state_clear = r"""if [[ $state != "" ]]; then clear_touchbar; fi
+        remove_and_unbind_keys;
+        state="";"""
+    
+    plugin_file.write(
+        definition +
+        initial_state_clear
+    )
+
+    for key in json_config['default']:
+        key_value = key
+        key_text = json_config['default'][key]['text']
+        function_name = json_config['default'][key]['command']
+        build_key(file, key_value, key_text, function_name)
+
+    function_termination = r'}'
+
+    plugin_file.write(function_termination)
 
 def build_key(file, value, text, func):
-    
+    key_command = f'set_key {key} "{key_text}" "{function_name}"'
+    file.write(key_command)
--- touchbar-extended.plugin.zsh
+++ touchbar-extended.plugin.zsh
@@ -1,6 +1,10 @@
 source ${0:A:h}/functions.zsh
 source ${0:A:h}/utility.zsh
 
+function generate_key() {
+
+}
+
 function display() {
     if [[ $state != "" ]]; then
         clear_touchbar
@@ -11,6 +15,8 @@ function display() {
     set_key 1 "🎋 `git_current_branch`" git_current_branch '-q'
     set_key 2 "status" "git status"
 }
+
+# TODO - second dynamic display function for handling when there are git functions in config
 
 precmd_touchbar_extended() {
     display
